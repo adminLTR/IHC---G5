7ad51b251cd4473fa87ab0b590ad34d7d54b0a9f
FranzARCH <130520660+FranzARCH@users.noreply.github.com>
2024-09-21 11:41:03 -0500

    Avance código esp32
    Código con un led, switch y potenciómetro

--- desarrollo/Hardware/Hardware.ino
+++ desarrollo/Hardware/Hardware.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+const int ledPin = 23; // led
+const int switchPin = 18; // switch
+const int potPin = 33; // potenciómetro
 
+int potValue = 0;           
+int ledBrightness = 0;       
+bool switchState = false;  
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  pinMode(switchPin, INPUT_PULLUP); 
+  Serial.begin(115200);    
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  switchState = digitalRead(switchPin); // leer el estado del switch
 
+  if (switchState == LOW) {              
+    potValue = analogRead(potPin); // leer el potenciómetro     
+    ledBrightness = map(potValue, 0, 4095, 0, 255); 
+    analogWrite(ledPin, ledBrightness); // ajustar brillo
+  } else {
+    analogWrite(ledPin, 0);
+  }
+
+  delay(10);  
 }
+
+// Simulación
+// https://wokwi.com/projects/409615387441850369 
